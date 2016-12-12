a71c0cf9f2611bd896f79c4208122f9bb9a81ddf
Michael Fladischer <FladischerMichael@fladi.at>
2016-12-12 11:29:23 +0100

    Use semver scheme.

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016022600;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = '0.0.0';        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;        // Requires this Moodle version
 $plugin->component = 'auth_telederm';       // Full name of the plugin (used for diagnostics)
